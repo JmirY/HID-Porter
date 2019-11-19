348e8c150e035d2280df388cb64da2d977ca0cdc
jmiry <yongseokj2@google.com>
2019-11-19 14:00:09 +0900

    Add mouse data porting method

--- src/SystemSwitch.cc
+++ src/SystemSwitch.cc
@@ -2,10 +2,11 @@
 // JMIRY
 #include "SystemSwitch.h"
 #include <iostream>
+#include <cstring>
 #include <fcntl.h>
 #include <unistd.h>
 
-#define BUF_SIZE 256
+#define BUF_SIZE 8
 
 void
 SystemSwitch::addSystem(System* sys)
@@ -19,18 +20,50 @@ SystemSwitch::addSystem(System* sys)
     m_systems.push_back(sys);
 }
 
-// void
-// SystemSwitch::deliver()
-// {
-//     char buf[BUF_SIZE];
-//     while (1)
-//     {
-//         read(m_host->getMouse(), buf, BUF_SIZE);
-//     }
-// }
+void
+SystemSwitch::portMouse()
+{
+    char buf[BUF_SIZE] = {0};
+    int host_fd, guest_fd;
+    int ret = 0, tmp = 0;
+
+    host_fd = m_systems[0]->getMouse();
+    while (1)
+    {
+        memset(buf, 0, BUF_SIZE);
+        // read data
+        ret = read(host_fd, buf, BUF_SIZE);
+        if (ret < 0)
+        {
+            perror("[ERR] Reading host mouse fd failed");
+            continue;
+        }
+
+        // check if user want to change data direction
+        // if 'm_active' is host(= 0) then do not send data
+        tmp = checkByte(buf);
+        if (tmp >= 0 && m_active != tmp)
+        {
+            m_active = tmp;
+            continue;
+        }
+
+        if (m_active)
+        {
+            // send data
+            guest_fd = m_systems[m_active]->getMouse();
+            ret = write(guest_fd, buf, BUF_SIZE);
+            if (ret < 0)
+            {
+                perror("[ERR] Writing guest mouse fd failed");
+                continue;
+            }
+        }
+    }
+}
 
 int
-SystemSwitch::checkByte(char* buf)
+SystemSwitch::checkByte(const char* buf)
 {
 
 }
