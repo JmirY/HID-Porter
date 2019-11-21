482705b6ef603b38116e0c5334d2f92ffc2217c8
jmiry <yongseokj2@google.com>
2019-11-21 12:26:07 +0900

    Make cleaner much smarter

--- util/gadget-cleaner.sh
+++ util/gadget-cleaner.sh
@@ -3,28 +3,43 @@
 
 #!bin/bash
 
+# USER MUST DEFINE THIS VARIABLE
+# define the number of gadget set (mouse & keyboard)
+num_of_gadget=2
+
+#--------Reusable information---------
 subsys_path="/config/usb_gadget"
-g_mouse_name="g_mouse"
-g_kbd_name="g_kbd"
-g_mouse_path="${subsys_path}/${g_mouse_name}"
-g_kbd_path="${subsys_path}/${g_kbd_name}"
 
-#-----clean up gadget mouse-----
-echo "" > "${g_mouse_path}/UDC"
-rmdir "${g_mouse_path}/strings/0x409"
+#--------clean up gadget mouse--------
+i=1
+while [ $i -le ${num_of_gadget} ]
+do
+    g_mouse_path="${subsys_path}/g_mouse$i"
+    echo "" > "${g_mouse_path}/UDC"
+    rmdir "${g_mouse_path}/strings/0x409"
 
-rm "${g_mouse_path}/configs/c.1/hid.usb0"
-rmdir "${g_mouse_path}/configs/c.1/strings/0x409"
-rmdir "${g_mouse_path}/configs/c.1"
-rmdir "${g_mouse_path}/functions/hid.usb0"
-rmdir "${g_mouse_path}"
+    rm "${g_mouse_path}/configs/c.1/hid.usb0"
+    rmdir "${g_mouse_path}/configs/c.1/strings/0x409"
+    rmdir "${g_mouse_path}/configs/c.1"
+    rmdir "${g_mouse_path}/functions/hid.usb0"
+    rmdir "${g_mouse_path}"
+    
+    i=`expr $i + 1`
+done
 
-#-----clean up gadget keyboard-----
-echo "" > "${g_kbd_path}/UDC"
-rmdir "${g_kbd_path}/strings/0x409"
+#------clean up gadget keyboard-------
+i=1
+while [ $i -le ${num_of_gadget} ]
+do
+    g_kbd_path="${subsys_path}/g_kbd$i"
+    echo "" > "${g_kbd_path}/UDC"
+    rmdir "${g_kbd_path}/strings/0x409"
 
-rm "${g_kbd_path}/configs/c.1/hid.usb0"
-rmdir "${g_kbd_path}/configs/c.1/strings/0x409"
-rmdir "${g_kbd_path}/configs/c.1"
-rmdir "${g_kbd_path}/functions/hid.usb0"
-rmdir "${g_kbd_path}"
+    rm "${g_kbd_path}/configs/c.1/hid.usb0"
+    rmdir "${g_kbd_path}/configs/c.1/strings/0x409"
+    rmdir "${g_kbd_path}/configs/c.1"
+    rmdir "${g_kbd_path}/functions/hid.usb0"
+    rmdir "${g_kbd_path}"
+
+    i=`expr $i + 1`
+done
